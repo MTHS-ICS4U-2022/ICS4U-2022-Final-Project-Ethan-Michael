6e112eda5ace4e78e682e2b00aa1edcdff2b7e88
Michael Clermont <Michael.Clermont@mths.ca>
2023-01-10 14:55:19 +0000

    Fixed click methods

--- js/gameScreen.js
+++ js/gameScreen.js
@@ -6,6 +6,8 @@
 // Created on: Dec 2022
 // This is the Game Scene
 
+import Square from './square.js'
+
 class GameScreen extends Phaser.Scene {
   preload() {
     // images
--- js/square.js
+++ js/square.js
@@ -15,20 +15,75 @@ const cellStates = {
   FIVE = 5,
   SIX = 6,
   SEVEN = 7,
-  EIGHT = 9,
-  COVERED = 10,
-  FLAGGED = 11,
-  MINE = 12,
-  BLOWNMINE = 13
+  EIGHT = 8,
+  COVERED = 9,
+  FLAGGED = 10,
+  MINE = 11,
+  BLOWNMINE = 12,
+  WRONGMINE = 13,
 }
 
-class Square extends Phaser.GameObjects.Sprite {
-  leftClick () {
-    game.input.mouse.capture = true;
-    if (game.input.activePointer.leftButton.isDown === true) {
-      game.stage.backgroundColor = "#4488AA";
+class SquareScreen extends Phaser.GameObjects.Sprite {
+  constructor(board, scene, x, y, xpos, ypos, mine) {
+    let default_state = cell_states.COVERED
+    super(scene, x, y, 'board', default_state)
+    scene.add.existing(this)
+    this.x = x
+    this.y = y
+    this.xpos = xpos
+    this.ypos = ypos
+    this.already_clicked = false
+    this.board = board
+    this.mined = mine
+    this.flagged = false
+    this.cellState = cellStates.COVERED
+    this.nearby_mines = 0
+  }
+
+  click () {
+    if (game.input.activePointer.rightButtonDown()) {
+      this.rightClick()
+    } else {
+      this.leftClick()
     }
+  }
+
+    leftClick () {
+      if (this.cellState != cellStates.FLAGGED) {
+        if (this.board.clickCount == 0) {
+          do {
+            this.board.createRandomMines();
+          } while (this.mined)
+        }
+        this.board.clickCount++
+      }
+
+      if (this.mined) {
+        for (let i = 0; i < this.board.cells.length; i++) {
+          for (let j = 0; j < this.board.cells[0].length; j++) {
+            if (this.board.cells[i][j].mined) {
+              this.board.cells[i][j].setState(cellStates.MINE);
+            } else if (this.board.cells[i][j].flagged) {
+              this.board.cells[i][j].setState(cellStates.WRONGMINE);
+            }
+          }
+        }
+        this.setState(cellStates.BLOWNMINE);
+      } else {
+        this.discoverBoard();
+      }
+  }
+
+  rightClick () {
+    if (this.cell_state == cellStates.COVERED || this.cell_state == cellStates.FLAGGED) {
+      this.flagged = !this.flagged;
+      this.setState(this.flagged ? cellStates.FLAGGED : cellStates.COVERED);
+    }
+  }
+
+  discoverBoard (flag) {
+    
   }
 }
 
-export default Square
+export default SquareScreen
